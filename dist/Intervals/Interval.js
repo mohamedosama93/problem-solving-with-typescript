23f0ed7c85397fab0e7d06a5c2c9b34f57a15e42
Mohamed Osama <mohammedosama.amagd@gmail.com>
2023-08-19 23:57:23 +0300

    add gitignore

--- dist/Intervals/Interval.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Interval {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
-    }
-}
-exports.default = Interval;
